542caff9ea7046ed7612d96f590fa526055ed47b
Sabrina <fernandezsabrina97@hotmail.com>
2021-03-25 15:50:37 -0300

    final commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var numero = 1 
+    while (numero <=30) {
+     if (numero % 2 != 0) {
+         console.log(numero)
+     }
+     numero= numero + 1
+    }
+
+    var nombres = ["Ainara", "Cristian", "Sabrina", "Carolina", "Marta"]
+    for (var i=0; i<nombres.length; i++) {
+        console.log(nombres[i])
+    }
+
+    var mascotas = [
+        "Budín", "Chulo", "Romeo", "Lola"
+    ]
+    console.log(mascotas)
+    mascotas.shift()
+    console.log(mascotas)
+
+    
